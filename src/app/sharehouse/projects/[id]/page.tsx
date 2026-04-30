d6fbbd57bdd893bd8ca807b8470d694dbe18eb6e
saigo112 <919saigo@gmail.com>
2026-04-30 21:19:16 +0900

    style: constrain project image width and center alignment in project details page

--- src/app/sharehouse/projects/[id]/page.tsx
+++ src/app/sharehouse/projects/[id]/page.tsx
@@ -103,7 +103,7 @@ export default async function ProjectDetailPage({ params }: { params: { id: stri
 
         <main>
           {project.image?.url && (
-            <div className="mb-12 rounded-3xl overflow-hidden shadow-2xl">
+            <div className="mb-12 rounded-3xl overflow-hidden shadow-2xl max-w-2xl mx-auto">
               {/* eslint-disable-next-line @next/next/no-img-element */}
               <img src={project.image.url} alt={project.title} className="w-full h-auto" />
             </div>
